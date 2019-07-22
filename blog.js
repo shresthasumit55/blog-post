648db50ce313bb58e7fee3fc8fe173cd175dc88f
Sumit Shrestha Khimbaja <SHRESTHAKHIMBAJ@ECS.BAYLOR.EDU>
2019-07-22 11:18:10 -0500

    added basic html

---
+++ blog.js
@@ -0,0 +1,26 @@
+$("document").ready(function () {
+    $("#page-home").show();
+    $("#page-archive").hide();
+    $("#page-about").hide();
+    $("#dialog-cmnt").hide();
+})
+
+$("#btn-home").click(function () {
+    $("#page-home").show();
+    $("#page-archive").hide();
+    $("#page-about").hide();
+})
+$("#btn-archive").click(function () {
+    $("#page-home").hide();
+    $("#page-archive").show();
+    $("#page-about").hide();
+})
+$("#btn-about").click(function () {
+    $("#page-home").hide();
+    $("#page-archive").hide();
+    $("#page-about").show();
+})
+
+$("#btn-search").click(function () {
+    $("#dialog-cmnt").show();
+})
