a5551737d067921c39c3f538230fc12a15787fe6
shresthasumit55 <shresthasumit55@gmail.com>
2019-08-01 08:16:16 -0500

    Update blog.js

--- blog.js
+++ blog.js
@@ -2,22 +2,32 @@ $("document").ready(function () {
     $("#page-home").show();
     $("#page-archive").hide();
     $("#page-about").hide();
+    $("#page-dialog").hide();
 })
 
 $("#btn-home").click(function () {
     $("#page-home").show();
     $("#page-archive").hide();
     $("#page-about").hide();
+    $("#page-dialog").hide();
 })
 $("#btn-archive").click(function () {
     $("#page-home").hide();
     $("#page-archive").show();
     $("#page-about").hide();
+    $("#page-dialog").hide();
 })
 $("#btn-about").click(function () {
     $("#page-home").hide();
     $("#page-archive").hide();
     $("#page-about").show();
+    $("#page-dialog").hide();
+})
+$("#btn-dialog").click(function () {
+    $("#page-home").hide();
+    $("#page-archive").hide();
+    $("#page-about").hide();
+    $("#page-dialog").show();
 })
 
 $("#btn-search").click(function () {
